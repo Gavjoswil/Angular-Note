3b8784c33354358395df6ee17feca4ca056fb238
Gavin Williams <gjosephw24@gmail.com>
2020-02-19 21:47:29 -0500

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,59 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatToolbarModule,} from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+
+import { AuthService } from './services/auth.service';
+import { LoginComponent } from './components/login/login.component';
+import { NotesService } from './services/notes.service';
+import { NoteIndexComponent } from './components/note/note-index/note-index.component';
+
+const routes = [
+  { path: 'register', component: RegistrationComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'notes', component: NoteIndexComponent},
+  { path: '**', component: RegistrationComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
+    HttpClientModule,
     BrowserModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AuthService,
+    NotesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/notes.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+
+const ApiUrl = 'http://kcpelevennoteapie.azurewebsites.net'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotesService {
+
+  constructor(private _http: HttpClient) { }
+
+  getNotes() {
+    return this._http.get(`${ApiUrl}/Notes`, {headers: this.getHeaders()});
+  }
+
+  private getHeaders() {
+    return new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('id_token')}`);
+  }
+}
